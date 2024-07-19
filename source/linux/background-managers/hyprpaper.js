0d6d2ae6e3d15114f7fc31501e1c20b2d18a3eec
VardanHeroic <tarayanvardan@gmail.com>
2024-07-19 11:19:02 +0400

    fixed wallpaper not setting bug

--- source/linux/background-managers/hyprpaper.js
+++ source/linux/background-managers/hyprpaper.js
@@ -1,5 +1,5 @@
 import childProcess from 'node:child_process';
-import {commandExists, execFile} from '../util.js';
+import {commandExists, execFile, exec} from '../util.js';
 
 export async function isAvailable() {
 	return commandExists('hyprpaper');
@@ -7,7 +7,7 @@ export async function isAvailable() {
 
 async function initialize() {
 	return new Promise(resolve => {
-		const hyprpaper = childProcess.spawn('script', ['-q', '-c', 'hyprpaper', '/dev/null']);
+		const hyprpaper = childProcess.spawn('script', ['--quiet', '--command', 'hyprpaper', '/dev/null']);
 		hyprpaper.stdout.on('data', data => {
 			if (data.toString().includes('[ERR]') || data.toString().includes('error')) {
 				throw new Error(data.toString());
@@ -19,21 +19,29 @@ async function initialize() {
 }
 
 export async function get() {
-	await initialize();
 	const {stdout: query} = await execFile('hyprctl', ['hyprpaper', 'listactive']);
 	if (query.includes('no wallpapers active')) {
 		return;
 	}
 
-	return query.split(' ')[2];
+	//	Query output looks like:
+	//	 eDP-1 = /path/to/image
+	//	 HDMI-A-1 = /path/to/image
+	return query.split(' ').at(-1).replace(/(\r\n|\n|\r)/gm, '');
 }
 
 export async function set(imagePath) {
 	await initialize();
+	const commandArray = [];
+	const {stdout: monitors} = await exec('hyprctl monitors | grep "Monitor" | awk \'{ print $2 }\'');
 	const {stdout: listloaded} = await execFile('hyprctl', ['hyprpaper', 'listloaded']);
 	if (!listloaded.includes(imagePath)) {
 		await execFile('hyprctl', ['hyprpaper', 'preload', imagePath]);
 	}
 
-	await execFile('hyprctl', ['hyprpaper', 'wallpaper', `,${imagePath}`]);
+	for (const monitor of monitors.split('\n').slice(0, -1)) {
+		commandArray.push(execFile('hyprctl', ['hyprpaper', 'wallpaper', `${monitor},${imagePath}`]));
+	}
+
+	await Promise.all(commandArray);
 }
