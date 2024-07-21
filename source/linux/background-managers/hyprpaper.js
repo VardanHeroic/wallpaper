dc0f1d67474cd4eaa84982ee2b80b5b7cd13340d
VardanHeroic <tarayanvardan@gmail.com>
2024-07-21 11:02:33 +0400

    simplifed monitor list getting

--- source/linux/background-managers/hyprpaper.js
+++ source/linux/background-managers/hyprpaper.js
@@ -7,7 +7,22 @@ export async function isAvailable() {
 
 async function initialize() {
 	return new Promise((resolve, reject) => {
-		const hyprpaper = childProcess.spawn('script', ['--quiet', '--command', 'hyprpaper', '/dev/null']);
+		const hyprpaper = childProcess.spawn('script', ['--return','--quiet', '--command', 'hyprpaper', '/dev/null']);
+		// const hyprpaper = childProcess.spawn('hyprpaper')
+		// hyprpaper.stderr.on('data', error => {
+		// 	console.log(error)
+		// 	resolve()
+		// })
+		// resolve()
+		// hyprpaper.on('close', code => {
+		// 	console.log(code);
+		// 	resolve()
+		// })
+		// hyprpapeR.on('error', err => {
+		// 	console.log(err);
+		// 	resolve()
+		// })
+		// resolve()
 		hyprpaper.stdout.on('data', data => {
 			if (data.toString().includes('[ERR]') || data.toString().includes('error')) {
 				reject(new Error(data.toString()));
@@ -19,6 +34,7 @@ async function initialize() {
 }
 
 export async function get() {
+	await initialize();
 	//	Query output looks like:
 	//	 eDP-1 = /path/to/image
 	//	  = /path/to/fallbackimage
@@ -28,7 +44,7 @@ export async function get() {
 		return;
 	}
 
-	const formattedQuery = query.split('\n').slice(0, -1).map(row => [row.split('=')[0], row.split('=').trim()]);
+	const formattedQuery = query.split('\n').slice(0, -1).map(row => [row.split('=')[0], row.split('=')[1].trim()]);
 	const fallBackImage = formattedQuery.find(row => row[0] === ' ')[1];
 
 	if (fallBackImage) {
@@ -40,52 +56,12 @@ export async function get() {
 
 export async function set(imagePath) {
 	await initialize();
-	const {stdout: monitors} = await execFile('hyprctl', ['monitors']);
-	// "hyprclt monitors" output looks like this:
-	//
-	// Monitor eDP-1 (ID 0):
-	//         1366x768@60.00300 at 0x0
-	//         description: Chimei Innolux Corporation 0x14C3
-	//         make: Chimei Innolux Corporation
-	//         model: 0x14C3
-	//         serial:
-	//         active workspace: 2 (2)
-	//         special workspace: 0 ()
-	//         reserved: 0 25 0 0
-	//         scale: 1.00
-	//         transform: 0
-	//         focused: yes
-	//         dpmsStatus: 1
-	//         vrr: 0
-	//         activelyTearing: false
-	//         disabled: false
-	//         currentFormat: XRGB8888
-	//         availableModes: 1366x768@60.00Hz
-	//
-	// Monitor HDMI-A-1 (ID 1):
-	//         1920x1080@60.00000 at 1366x0
-	//         description:
-	//         make:
-	//         model:
-	//         serial:
-	//         active workspace: 4 (4)
-	//         special workspace: 0 ()
-	//         reserved: 0 50 0 0
-	//         scale: 2.00
-	//         transform: 0
-	//         focused: no
-	//         dpmsStatus: 1
-	//         vrr: 0
-	//         activelyTearing: false
-	//         disabled: false
-	//         currentFormat: XRGB8888
-	//         availableModes:
-	//
-	//
+	const {stdout: jsonMonitors} = await execFile('hyprctl', ['monitors', '-j'])
+	const monitors = JSON.parse(jsonMonitors)
 	const {stdout: listloaded} = await execFile('hyprctl', ['hyprpaper', 'listloaded']);
 	if (!listloaded.includes(imagePath)) {
 		await execFile('hyprctl', ['hyprpaper', 'preload', imagePath]);
 	}
 
-	await Promise.all(monitors.split('\n').flatMap(monitor => monitor.includes('Monitor') ? [execFile('hyprctl', ['hyprpaper', 'wallpaper', `${monitor.split(' ')[1]}, ${imagePath}`])] : []));
+	await Promise.all(monitors.map(monitor => execFile('hyprctl', ['hyprpaper', 'wallpaper', `${monitor.name}, ${imagePath}`])));
 }
